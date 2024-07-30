0d89609fa585606e1e44e5ed38413bd75f63de21
masnin15 <masninjuairia10@gmail.com>
2024-07-31 04:19:20 +0600

    Tiny issues

--- checkout.php
+++ checkout.php
@@ -56,7 +56,8 @@ if (isset($_POST['order_btn'])) {
 <html lang='en'>
 <head>
     <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.θ'>
+    <link  rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'>
     <link rel='stylesheet' type='text/css' href='main.css'>
     <title>Checkout page</title>
